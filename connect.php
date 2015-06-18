d8b72b16911935ae851312299e40b6b63cfe556f
Blackdeathhh <blackdeath44@gmail.com>
2015-06-18 03:35:42 +0000

    tets connection
    Connection commit

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 	require_once('db.php');
-if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
+if(!$dbconn = mysql_connect("", "", "")) {
 	echo 'Could not connect to mysql on ' . DB_HOST . '\n';
 exit;
 }
 echo 'Connected to mysql <br />'
 ;
-if(!mysql_select_db(DB_NAME, $dbconn)) {
+if(!mysql_select_db("", $dbconn)) {
 	echo 'Could not user database ' . DB_NAME . '\n';
 	echo mysql_error() . '\n';
 	exit;
